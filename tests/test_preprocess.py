a4dfaa1f5daecf1d13eba7cbc19903d33159c761
Zecro <adrianfingal@gmail.com>
2024-01-17 15:55:22 +0100

    Added docs for test of preprocess

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,3 +1,9 @@
+""" 
+This file contains tests for the preprocess scripts. It uses the pytest library to run the tests.
+To run simply execute the following command in the terminal:
+> pytest scripts/test_preprocess.py
+"""
+
 from pathlib import Path
 
 import pytest
@@ -9,6 +15,15 @@ from scripts.preprocess import main
 
 @pytest.fixture
 def input_path(tmp_path):
+    """
+    Create a temporary input file and return its path.
+
+    Args:
+        tmp_path (str): The path to the temporary directory.
+
+    Returns:
+        str: The path to the created input file.
+    """
     # Create a temporary input file
     input_file = tmp_path / "input.json"
     input_file.write_text('[{"text": "example text", "sni": "example_label"}]')
@@ -16,10 +31,28 @@ def input_path(tmp_path):
 
 @pytest.fixture
 def output_path(tmp_path):
+    """
+    Returns the path of a temporary output file.
+
+    Args:
+        tmp_path (str): The path of the temporary directory.
+
+    Returns:
+        str: The path of the temporary output file.
+    """
     # Create a temporary output file
     return tmp_path / "test.spacy"
 
 def test_main(input_path, output_path):
+    """
+    Test the main function by calling it with the test input and output paths.
+    Check if the output directory exists and if the saved documents were processed and saved correctly.
+    Clean up the temporary files after the test.
+    
+    Args:
+        input_path (str): The path to the test input file.
+        output_path (str): The path to the test output directory.
+    """
     # Call the main function with the test input and output paths
     main(input_path, output_path)
 
@@ -40,5 +73,3 @@ def test_main(input_path, output_path):
     input_path.unlink()
     output_path.unlink()
     
-# Run the test with pytest
-# $ pytest scripts/test_preprocess.py
