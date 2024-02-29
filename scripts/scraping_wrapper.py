c5d8ee0e4c1241805f3ed67b1578e32f82b5a475
Hugo Hedlund <48281171+Hedlund01@users.noreply.github.com>
2024-02-29 11:57:06 +0100

    Refactor scraping_wrapper.py to use MongoDB for retrieving start URLs
    Co-authored-by: F1sshy <F1sshy@users.noreply.github.com>

--- scripts/scraping_wrapper.py
+++ scripts/scraping_wrapper.py
@@ -8,21 +8,16 @@ import json
 import logging
 import subprocess
 from pathlib import Path
+
 import typer
 from typing_extensions import Annotated
+
 from definitions import ROOT_DIR
+from scripts.mongo import get_client
+from scripts.scb import Schema
 
 
 def main(
-        input_path: Annotated[Path, typer.Argument(
-            exists=True, 
-            file_okay=True, 
-            dir_okay=False, 
-            readable=True, 
-            resolve_path=True, 
-            formats=["json"], 
-            help="The path to the input data file."
-            )],
         output_path: Annotated[Path, typer.Argument(
             exists=True, 
             file_okay=True, 
@@ -33,25 +28,24 @@ def main(
             help="The path to the output data file."
             )],
         ):
-        """
-        Run Scrapy crawler with the given start URLs and output file path.
-
-        :param input_path (Path): The path to the input file, which comes from google_wrapper.py.
-        :param output_file_path (Path): The path to the output file.
-        """
         
-        with open(input_path, 'r', encoding='utf-8') as f:
-            logging.debug("Reading data from %s", input_path)
-            data = json.load(f)
+        client = get_client()
+        query = {"url": { "$regex" : "^(?!\\s*$).+" }}
+        
+    
+        documents = client[Schema.DB][Schema.COMPANIES].find(query)
+
         start_urls: str = ""
-        logging.debug("Looping through the data to get the start URLs for the Scrapy crawler.")
-        for index, company in enumerate(data):
-            if (company["url"] != "" or company["url"] != None):
-                logging.debug("Adding company url '%s' to be crawled.", company["url"])
-                if index == len(data) - 1:
-                    start_urls += company["url"]
-                else:
-                    start_urls += company["url"] + ","
+        document_length = len(list(documents))
+        
+        for index, company in enumerate(documents):
+            if index == document_length - 1:
+                start_urls += f'"{company["url"]}"'
+            else:
+                start_urls += f'"{company["url"]}",'
+            
+            
+
 
         output_path = ROOT_DIR / output_path
         command = f'scrapy crawl crawlingNLP -a start_urls={start_urls} -o {output_path}:json'.split(" ")
