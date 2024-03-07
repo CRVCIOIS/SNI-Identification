a7a15ea8c29517a043a5f788e299b72071b3cc7e
yuvve <37124628+yuvve@users.noreply.github.com>
2024-03-07 15:46:13 +0100

    Small fix

--- pipeline/scrape.py
+++ pipeline/scrape.py
@@ -10,10 +10,10 @@ def main(scrape_output_folder: Path = typer.Argument(..., dir_okay=True)):
     scb_adapter = SCBAdapter()
 
     companies = scb_adapter.fetch_all_companies_from_db(no_url=True)
-    start_urls = [company["url"] for company in companies]
+    start_urls = [{'label':company['org_nr'], 'url':company["url"]} for company in companies]
 
-    scraper = Scraper()
-    scraper.scrape_all(scrape_output_folder, start_urls)
+    scraper = Scraper(scrape_output_folder)
+    scraper.scrape_all(start_urls)
 
 if __name__ == "__main__":
     typer.run(main)
